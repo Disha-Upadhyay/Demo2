f8c5161ec209150b08c1e6932a88cca95d11f0f4
Disha-Upadhyay <dishaupadhyay2007@gmail.com>
2026-01-15 14:53:15 +0530

    merge conflict resolve

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,7 @@
 public class HelloWorld{
      public static void main(String[]args){
-<<<<<<< HEAD
 	 system.out.println("HelloWorld updated by feature1");
-=======
 	 system.out.println("HelloWorld updated by main");
->>>>>>> main
      system.out.println("Hello feature1");
 	 }
 	 }
